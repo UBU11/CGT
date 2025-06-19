17875cd6be2e5dc459aa86ac83835dfca3e2c01e
UBU11 <uvaisubu11@gmail.com>
2025-06-20 01:34:22 +0530

    simple condition added

--- src/CGT.js
+++ src/CGT.js
@@ -1,10 +1,30 @@
 const btn = document.querySelectorAll("button");
-const display = document.getElementById("display")
+const display = document.getElementById("display");
+let randNum = Math.floor(Math.random() * 10);
+display.innerText= randNum
 
 btn.forEach((item)=>{
     item.addEventListener("click",(e)=>{
-        console.log("clicked",e.target.textContent)
+        console.log("clicked",e.target.textContent)  
         display.innerText= e.target.textContent;
+        item.style.background = "#B6F500"
+        if(e.target.textContent == randNum){
+            display.innerText = "TRUE"
+            
+        }
+        else{
+            display.innerText = "Try Again"
+            item.style.background = "red"
+             if(display.innerText != "TRUE")
+            setInterval(() => {
+                display.innerText= randNum
+                item.style.background = "#0093b8"
+            }, 2000);
+        }
+
     })
 })
+
+
+
 
